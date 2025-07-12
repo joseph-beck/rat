ffc02d4c39dc783f36ee84674bc9d441a19578a2
Joseph Beck <joebeck03@icloud.com>
2025-07-12 12:09:57 +0100

    feat: update from depracted vite extension

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite({ autoCodeSplitting: true }), tailwindcss()],
+  plugins: [
+    react(),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    tailwindcss(),
+  ],
 });

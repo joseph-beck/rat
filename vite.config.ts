420936607d73d7806050a7f249b0c3b9ceb8e7b4
Joseph Beck <96671050+joseph-beck@users.noreply.github.com>
2026-02-17 14:26:21 +0000

    feat: can now use pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react-swc'
+import fs from 'fs'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
+const https =
+  fs.existsSync('./key.pem') && fs.existsSync('./cert.pem')
+    ? {
+        key: fs.readFileSync('./key.pem'),
+        cert: fs.readFileSync('./cert.pem'),
+      }
+    : undefined
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    https: https,
+  },
   plugins: [
     tanstackRouter({
       target: 'react',
@@ -12,5 +25,35 @@ export default defineConfig({
     }),
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'rat-pwa',
+        short_name: 'rat-pwa',
+        start_url: '.',
+        description: 'react app template progressive web app',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '192x192-icon.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '512x512-icon.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+        ],
+      },
+    }),
   ],
 })

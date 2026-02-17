49929d8888a35f1739fca9884152a40fc0ecbe96
Joseph Beck <joebeck03@icloud.com>
2026-02-17 10:12:45 +0000

    feat: add import sort

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+import './styles/global.css';
+
+import { createRouter,RouterProvider } from '@tanstack/react-router';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
-import './styles/global.css';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
